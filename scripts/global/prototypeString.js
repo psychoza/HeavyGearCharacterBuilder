86d24c962c2dbfe9ce558662655f64056921636c
psychoza <dragon26>
2014-11-11 11:11:24 -0500

    Added pad to string prototype
    :doughnut:

--- scripts/global/prototypeString.js
+++ scripts/global/prototypeString.js
@@ -7,3 +7,28 @@ String.prototype.rtrim = function () {
 String.prototype.trim = function() {
     return this.ltrim().rtrim();
 };
+String.prototype.pad = function(string, length, character, direction) {
+	var STR_PAD_LEFT = 1;
+	var STR_PAD_RIGHT = 2;
+	var STR_PAD_BOTH = 3;
+    if (typeof(length) == "undefined") { var length = 0; }
+    if (typeof(character) == "undefined") { var character = ' '; }
+    if (typeof(direction) == "undefined") { var direction = STR_PAD_RIGHT; }
+
+    if (length + 1 >= string.length) {
+        switch (direction){
+            case STR_PAD_LEFT:
+                string = Array(length + 1 - string.length).join(character) + string;
+            	break;
+            case STR_PAD_BOTH:
+                var right = Math.ceil((padlen = length - string.length) / 2);
+                var left = padlen - right;
+                string = Array(left+1).join(character) + string + Array(right+1).join(character);
+            	break;
+            default:
+                string = string + Array(length + 1 - string.length).join(character);
+            	break;
+        }
+    }
+    return string;
+}
