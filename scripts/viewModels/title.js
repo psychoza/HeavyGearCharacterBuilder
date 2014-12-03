0a908893774012303560422b2728ba2b228ef7e0
Carnes <whoismaha@gmail.com>
2014-12-03 16:20:23 -0500

    fixed title not updating on load

--- scripts/viewModels/title.js
+++ scripts/viewModels/title.js
@@ -4,14 +4,16 @@ var Title = (function(){
     var baseText = 'Heavy Gear Character Builder';
 
     self.text = ko.observable(baseText);
+    var updateName = function(name){
+        if(name.length > 0) {
+            self.text(name + ' :: ' + baseText);
+            return;
+        }
+        self.text(baseText);
+    };
     self.watchCharacterName = function(characterName){
-        characterName.subscribe(function(name){
-            if(name.length > 0) {
-                self.text(name + ' :: ' + baseText);
-                return;
-            }
-            self.text(baseText);
-        });
+        characterName.subscribe(updateName);
+        updateName(characterName());
     };
 
     return self;
